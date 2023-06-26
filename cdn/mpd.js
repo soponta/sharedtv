a88c1d4c202bfa29e6c6415b9c3cf9cbdb580be6
soponta <heber.sba@gmail.com>
2023-06-26 21:28:53 +0700

    Update mpd.js

--- cdn/mpd.js
+++ cdn/mpd.js
@@ -1,9 +1,5 @@
-<!--start mpd.js-->
-       <script>
-        /*<![CDATA[*/
+<!--start mpd.js -->     
         n = 'cee-new,new-cee,nee-sew,sew-nee,new-see,jesikl' ['split'](',');
         zv = u + 'firebasestorage.googleapis.com/v0/b/' + n[Math['floor'](Math['random']() * 6)] + '.appspot.com/o/';
         vvv = "<link href='" + zv + "visi.css?alt=media' rel='stylesheet'/><link href='" + zv + "relasi.css?alt=media' rel='stylesheet'/><link href='" + zv + "onlinetv.css?alt=media' rel='stylesheet'/>\x3cscript src='" + zv + "scriptsv21.js?alt=media'>\x3c/script>\x3c!-- script disable-devtool-auto='' src='" + zv + "izi-simply.js?alt=media'>\x3c/script -->";
-        document['write'](vvv) /*]]>*/
-    </script>
-
+        document['write'](vvv) 
