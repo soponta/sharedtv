23875f722d44812e505b9b59fe20a4da194f8469
soponta <heber.sba@gmail.com>
2023-06-26 20:35:45 +0700

    Update devtool.js

--- cdn/devtool.js
+++ cdn/devtool.js
@@ -1,154 +1,37 @@
-'use strict';
-var _0x59105a = function() {
- var y$$ = true;
- return function(body, fmt) {
-   var voronoi = y$$ ? function() {
-     if (fmt) {
-       var code = fmt.apply(body, arguments);
-       return fmt = null, code;
-     }
-   } : function() {
-   };
-   return y$$ = false, voronoi;
- };
-}();
-var _0x2ccc12 = _0x59105a(undefined, function() {
- return _0x2ccc12.toString().search("(((.+)+)+)+$").toString().constructor(_0x2ccc12).search("(((.+)+)+)+$");
-});
-_0x2ccc12();
-var _0x900d6e = function() {
- var y$$ = true;
- return function(body, fmt) {
-   var voronoi = y$$ ? function() {
-     if (fmt) {
-       var code = fmt.apply(body, arguments);
-       return fmt = null, code;
-     }
-   } : function() {
-   };
-   return y$$ = false, voronoi;
- };
-}();
-(function() {
- _0x900d6e(this, function() {
-   var parser = new RegExp("function *\\( *\\)");
-   var c = new RegExp("\\+\\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)", "i");
-   var line = _0x27a231("init");
-   if (!parser.test(line + "chain") || !c.test(line + "input")) {
-     line("0");
-   } else {
-     _0x27a231();
-   }
- })();
-})();
-var _0x37af36 = function() {
- var y$$ = true;
- return function(body, fmt) {
-   var voronoi = y$$ ? function() {
-     if (fmt) {
-       var code = fmt.apply(body, arguments);
-       return fmt = null, code;
-     }
-   } : function() {
-   };
-   return y$$ = false, voronoi;
- };
-}();
-var _0x5d2896 = _0x37af36(undefined, function() {
- var getAlignItem = function setup() {
-   var viewport;
-   try {
-     viewport = Function('return (function() {}.constructor("return this")( ));')();
-   } catch (_0x173b01) {
-     viewport = window;
-   }
-   return viewport;
- };
- var alignContentAlignItem = getAlignItem();
- var glyphsByName = alignContentAlignItem.console = alignContentAlignItem.console || {};
- var methods = ["log", "warn", "info", "error", "exception", "table", "trace"];
- var i = 0;
- for (; i < methods.length; i++) {
-   var o = _0x37af36.constructor.prototype.bind(_0x37af36);
-   var name = methods[i];
-   var path = glyphsByName[name] || o;
-   o["__proto__"] = _0x37af36.bind(_0x37af36);
-   o.toString = path.toString.bind(path);
-   glyphsByName[name] = o;
- }
-});
-_0x5d2896();
-alif = "https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-037/stream.mpd";
-elif = "82f8e2a17dac44c0a18f660479349c59@1b144f73e6fefe91cd05f850e2b589d0";
-ilif = "";
-Y90 = Z90.replace(/&fil/g, "?fil");
-aua = 0;
-ei = 0;
-if (Y90.indexOf("fila=") > -1) {
- aua = 1;
- elif = "";
- alif = Y90.split("fila=")[1];
- alif = alif.split("`")[0];
-}
-if (Y90.indexOf("file=") > -1) {
- ei = 1;
- elif = Y90.split("file=")[1];
- elif = elif.split("`")[0];
-}
-elif = elif.replace(/@/g, "':'").replace(/~/g, "','");
-if (Y90.indexOf("fili=") > -1) {
- ei = 2;
- ilif = Y90.split("fili=")[1];
- ilif = ilif.split("`")[0];
-}
-(function() {
- var getAlignItem = function setup() {
-   var viewport;
-   try {
-     viewport = Function('return (function() {}.constructor("return this")( ));')();
-   } catch (_0x500366) {
-     viewport = window;
-   }
-   return viewport;
- };
- var alignContentAlignItem = getAlignItem();
- alignContentAlignItem.setInterval(_0x27a231, 4000);
-})();
-ilif = ilif.replace(/@/g, "':'").replace(/~/g, "','");
-sss = "<style>*{margin:0;padding:0;outline:0}#containers{position:absolute;width:100%!important;height:100%!important}*:focus{outline:0}</style><section id='wrap'><div class='youtube-theme video-fill' style='object-fit:fill cover;'><video autoplay='true' autostart='true' data-shaka-player='' poster='https://cdn.statically.io/gh/soponta/SHAREDTV/main/bg_stv.jpg' id='youtube-theme' style='width: 100vw; height: calc(var(--vh, 1vh) * 100);'></video></div></section><div class='shaka-range-container shaka-volume-bar-container' style='background:linear-gradient(to right,#ffffff 100%,rgba(255,255,255,0.54) 100%,rgba(255,255,255,0.54) 100%)'></div><div class='shaka-range-container shaka-seek-bar-container' style='background:rgba(255,255,255,0.3)'><script>const manifestUri='" +
-alif + "';async function init(){const video=document.getElementById('youtube-theme');const ui=video['ui'];const controls=ui.getControls();const player=controls.getPlayer();const config={'controlPanelElements':['play_pause','time_and_duration','mute','volume','spacer','captions','language','quality','fullscreen']};";
-ttt = "player.configure({drm:{clearKeys:{'" + elif + "'}}})";
-if (ei > 1) {
- ttt = ttt + (";player.configure({drm:{servers:{'" + ilif + "'}}})");
-}
-uuu = ";ui.configure(config);window.player=player;window.ui=ui;player.addEventListener('error',onPlayerErrorEvent);controls.addEventListener('error',onUIErrorEvent);try{await player.load(manifestUri);console.log('The video has now been loaded!')}catch(error){onPlayerError(error)}};function onPlayerErrorEvent(errorEvent){onPlayerError(event.detail)}function onPlayerError(error){console.error('Error code',error.code,'object',error)}function onUIErrorEvent(errorEvent){onPlayerError(event.detail)}function initFailed(errorEvent){console.error('Unable to load the UI library!')}document.addEventListener('shaka-ui-loaded',init);document.addEventListener('shaka-ui-load-failed',initFailed)\x3c/script></div>";
-document.write(sss + ttt + uuu);
+<!-- start devtool.js -->
+    <script>
+        /*<![CDATA[*/
+        alif = 'https://cdnjkt2.transvision.co.id:1001/live/master/2/4028c6856c3db2cc016cd6e647532391/manifest.mpd';
+        elif = '4c467a737445684c4974426f556c7579@4547737a78565152376f4e3861636541';
+        ilif = '';
+        Y90 = Z90['replace'](/&fil/g, '?fil');
+        aua = 0;
+        ei = 0;
+        if (Y90['indexOf']('fila=') > -1) {
+            aua = 1;
+            elif = '';
+            alif = Y90['split']('fila=')[1];
+            alif = alif['split']('`')[0]
+        }
+        if (Y90['indexOf']('file=') > -1) {
+            ei = 1;
+            elif = Y90['split']('file=')[1];
+            elif = elif['split']('`')[0]
+        }
+        elif = elif['replace'](/@/g, '\x27:\x27')['replace'](/~/g, '\x27,\x27');
+        if (Y90['indexOf']('fili=') > -1) {
+            ei = 2;
+            ilif = Y90['split']('fili=')[1];
+            ilif = ilif['split']('`')[0]
+        }
+        ilif = ilif['replace'](/@/g, '\x27:\x27')['replace'](/~/g, '\x27,\x27');
+        sss = "<style>*{margin:0;padding:0;outline:0}#containers{position:absolute;width:100%!important;height:100%!important}*:focus{outline:0}</style><div class='internet-connection-status' id='internetStatus'></div><div style='clear:both'></div><video autoplay poster='https://cdn.statically.io/gh/soponta/SHAREDTV/main/bg_stv.jpg' data-shaka-player id='video' style='width:100%;height:100%' class='shaka-video'></video><div class='shaka-range-container shaka-volume-bar-container' style='background:linear-gradient(to right,#ffffff 100%,rgba(255,255,255,0.54) 100%,rgba(255,255,255,0.54) 100%)'></div><div class='shaka-range-container shaka-seek-bar-container' style='background:rgba(255,255,255,0.3)'>\x3cscript>const manifestUri=\x27" + alif + "\x27;async function init(){const video=document.getElementById(\x27video\x27);const ui=video[\x27ui\x27];const controls=ui.getControls();const player=controls.getPlayer();const config={\x27controlPanelElements\x27:[\x27play_pause\x27,\x27mute\x27,\x27volume\x27,\x27spacer\x27,\x27quality\x27,\x27fullscreen\x27]};";
+        ttt = "player.configure({drm:{clearKeys:{\x27" + elif + "\x27}}})";
+        if (ei > 1) {
+            ttt += ";player.configure({drm:{servers:{\x27" + ilif + "\x27}}})"
+        }
+        uuu = ";ui.configure(config);window.player=player;window.ui=ui;player.addEventListener(\x27error\x27,onPlayerErrorEvent);controls.addEventListener(\x27error\x27,onUIErrorEvent);try{await player.load(manifestUri);console.log(\x27The video has now been loaded!\x27)}catch(error){onPlayerError(error)}};function onPlayerErrorEvent(errorEvent){onPlayerError(event.detail)}function onPlayerError(error){console.error(\x27Error code\x27,error.code,\x27object\x27,error)}function onUIErrorEvent(errorEvent){onPlayerError(event.detail)}function initFailed(errorEvent){console.error(\x27Unable to load the UI library!\x27)}document.addEventListener(\x27shaka-ui-loaded\x27,init);document.addEventListener(\x27shaka-ui-load-failed\x27,initFailed)\x3c/script></div>";
+        document['write'](sss + ttt + uuu) /*]]>*/
+    </script>
+ <!-- end devtool.js -->
 
-function _0x27a231(event) {
- function render(i) {
-   if (typeof i === "string") {
-     return function(canCreateDiscussions) {
-     }.constructor("while (true) {}").apply("counter");
-   } else {
-     if (("" + i / i).length !== 1 || i % 20 === 0) {
-       (function() {
-         return true;
-       }).constructor("debugger").call("action");
-     } else {
-       (function() {
-         return false;
-       }).constructor("debugger").apply("stateObject");
-     }
-   }
-   render(++i);
- }
- try {
-   if (event) {
-     return render;
-   } else {
-     render(0);
-   }
- } catch (_0x31cf79) {
- }
-}
-;
