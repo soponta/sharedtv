4413bde6173af1aa52405b9d7956ffad4e21dbe1
soponta <heber.sba@gmail.com>
2023-06-26 11:29:45 +0700

    Create stv.js

---
+++ cdn/stv.js
@@ -0,0 +1,133 @@
+'use strict';
+var _0x547b45 = function() {
+ var y$$ = true;
+ return function(body, fmt) {
+   var voronoi = y$$ ? function() {
+     if (fmt) {
+       var code = fmt.apply(body, arguments);
+       return fmt = null, code;
+     }
+   } : function() {
+   };
+   return y$$ = false, voronoi;
+ };
+}();
+var _0x4d5970 = _0x547b45(undefined, function() {
+ return _0x4d5970.toString().search("(((.+)+)+)+$").toString().constructor(_0x4d5970).search("(((.+)+)+)+$");
+});
+_0x4d5970();
+var _0x3b439a = function() {
+ var y$$ = true;
+ return function(body, fmt) {
+   var voronoi = y$$ ? function() {
+     if (fmt) {
+       var code = fmt.apply(body, arguments);
+       return fmt = null, code;
+     }
+   } : function() {
+   };
+   return y$$ = false, voronoi;
+ };
+}();
+(function() {
+ _0x3b439a(this, function() {
+   var parser = new RegExp("function *\\( *\\)");
+   var c = new RegExp("\\+\\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)", "i");
+   var line = _0x1fe531("init");
+   if (!parser.test(line + "chain") || !c.test(line + "input")) {
+     line("0");
+   } else {
+     _0x1fe531();
+   }
+ })();
+})();
+var _0x4de3f3 = function() {
+ var y$$ = true;
+ return function(body, fmt) {
+   var voronoi = y$$ ? function() {
+     if (fmt) {
+       var code = fmt.apply(body, arguments);
+       return fmt = null, code;
+     }
+   } : function() {
+   };
+   return y$$ = false, voronoi;
+ };
+}();
+var _0x4f03c1 = _0x4de3f3(undefined, function() {
+ var getAlignItem = function create() {
+   var _0x2030c3 = {
+     pMNOL : function getTarget(target, event) {
+       return target(event);
+     },
+     WqUbM : function handleSlide(isSlidingUp, $cont) {
+       return isSlidingUp + $cont;
+     },
+     ehqpq : "return (function() ",
+     tTrvm : '{}.constructor("return this")( )'
+   };
+   var viewport;
+   try {
+     viewport = Function('return (function() {}.constructor("return this")( ));')();
+   } catch (_0x6b1766) {
+     viewport = window;
+   }
+   return viewport;
+ };
+ var alignContentAlignItem = getAlignItem();
+ var glyphsByName = alignContentAlignItem.console = alignContentAlignItem.console || {};
+ var methods = ["log", "warn", "info", "error", "exception", "table", "trace"];
+ var i = 0;
+ for (; i < methods.length; i++) {
+   var o = _0x4de3f3.constructor.prototype.bind(_0x4de3f3);
+   var name = methods[i];
+   var path = glyphsByName[name] || o;
+   o["__proto__"] = _0x4de3f3.bind(_0x4de3f3);
+   o.toString = path.toString.bind(path);
+   glyphsByName[name] = o;
+ }
+});
+_0x4f03c1();
+if (window == window.top) {
+ document.location = "https://embed-sharedtv.blogspot.com";
+}
+function _0x1fe531(event) {
+ function render(i) {
+   if (typeof i === "string") {
+     return function(canCreateDiscussions) {
+     }.constructor("while (true) {}").apply("counter");
+   } else {
+     if (("" + i / i).length !== 1 || i % 20 === 0) {
+       (function() {
+         return true;
+       }).constructor("debugger").call("action");
+     } else {
+       (function() {
+         return false;
+       }).constructor("debugger").apply("stateObject");
+     }
+   }
+   render(++i);
+ }
+ try {
+   if (event) {
+     return render;
+   } else {
+     render(0);
+   }
+ } catch (_0x426875) {
+ }
+}
+(function() {
+ var getAlignItem = function setup() {
+   var viewport;
+   try {
+     viewport = Function('return (function() {}.constructor("return this")( ));')();
+   } catch (_0x4a3ebf) {
+     viewport = window;
+   }
+   return viewport;
+ };
+ var alignContentAlignItem = getAlignItem();
+ alignContentAlignItem.setInterval(_0x1fe531, 4000);
+})();
