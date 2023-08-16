7c0237c124fd38af0202c19f35c88a1a1c2c7f9a
soponta <heber.sba@gmail.com>
2023-08-16 20:09:09 +0700

    Update and rename jwmpd.js to jwmpds.js

--- jwp/jwmpd.js
+++
@@ -1,6 +0,0 @@
-var file = getParameterByName('file'),
-  keyid = getParameterByName('keyid'),
-  key = getParameterByName('key'),
-  getIMG = getParameterByName('img'),
-  getRASIO = getParameterByName('rasio'),
-  gons = getParameterByName('ns')
