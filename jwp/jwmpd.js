526ac49b460654631f19965a07929c86898beda1
soponta <heber.sba@gmail.com>
2023-08-16 19:41:38 +0700

    Update jwmpd.js

--- jwp/jwmpd.js
+++ jwp/jwmpd.js
@@ -1,3 +1,8 @@
+ g = g.replace(/[\[]/, '[').replace(/[\]]/, ']')
+  var J = new RegExp('[?&]' + g + '=([^&#]*)'),
+    N = J.exec(location.search)
+  return N === null ? '' : decodeURIComponent(N[1].replace(/\+/g, ' '))
+}
 var file = getParameterByName('file'),
   keyid = getParameterByName('keyid'),
   key = getParameterByName('key'),
